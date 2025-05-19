5547ab56214ba70a5c3081df77c33fc0ccbae67e
Steve Rosam <steve+newdemo@quix.io>
2025-05-19 15:46:59 +0000

    Edited 'main.py'

--- enrich/main.py
+++ enrich/main.py
@@ -39,8 +39,8 @@ sdf.print()
 
 # Create nice JSON alert message.
 sdf = sdf.apply(lambda row: {
-    "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
-    "Alert": "For last 1 second, average braking power was " + str(row["value"]),
+    "Timestamp": str(datetime.fromtimestamp(row["Timestamp"]/1000)),
+    "Alert": "For last 1 second, average speed was " + str(row["Speed"]),
     "Speed": row["Speed"],
     "config_id": current_config_id
 })
