9c15c9b3d457e3cf63bc0ccf7662cfaa761367b7
Steve Rosam <steve+newdemo@quix.io>
2025-05-19 15:26:26 +0000

    Edited 'main.py'

--- enrich/main.py
+++ enrich/main.py
@@ -22,17 +22,19 @@ sdf = sdf.apply(lambda row: float(row["Brake"])) \
         
 sdf.print()
 
-# Filter only windows where average brake force exceeded 50%.
-sdf = sdf[sdf["value"] > 0.5]
+
+# GET AND CACHE THE CURRENT CONFIG
+current_config_id = 1
+
 
 # Create nice JSON alert message.
-# sdf = sdf.apply(lambda row: {
-#     "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
-#     "Alert": {
-#         "Title": "Hard braking detected.",
-#         "Message": "For last 1 second, average braking power was " + str(row["value"])
-#     }
-# })
+sdf = sdf.apply(lambda row: {
+    "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
+    "Alert": {
+        "Title": "Hard braking detected.",
+        "Message": "For last 1 second, average braking power was " + str(row["value"])
+    }
+})
 
 # Print JSON messages in console.
 sdf.print()
