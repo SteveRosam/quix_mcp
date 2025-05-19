617e578810f279d5b5fa931b971ecae7bd91a943
Steve Rosam <steve+newdemo@quix.io>
2025-05-19 15:23:40 +0000

    Edited 'main.py'

--- enrich/main.py
+++ enrich/main.py
@@ -26,19 +26,19 @@ sdf.print()
 sdf = sdf[sdf["value"] > 0.5]
 
 # Create nice JSON alert message.
-sdf = sdf.apply(lambda row: {
-    "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
-    "Alert": {
-        "Title": "Hard braking detected.",
-        "Message": "For last 1 second, average braking power was " + str(row["value"])
-    }
-})
+# sdf = sdf.apply(lambda row: {
+#     "Timestamp": str(datetime.fromtimestamp(row["start"]/1000)),
+#     "Alert": {
+#         "Title": "Hard braking detected.",
+#         "Message": "For last 1 second, average braking power was " + str(row["value"])
+#     }
+# })
 
 # Print JSON messages in console.
 sdf.print()
 
 # Send the message to the output topic
-sdf.to_topic(output_topic)
+# sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run()
